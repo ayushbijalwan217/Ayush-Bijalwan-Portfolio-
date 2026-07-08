826a201b780686051a37dfaeb2bd40643b5719ef
ayushbijalwan217 <ayushbijalwan217@gmail.com>
2026-07-08 15:04:30 +0530

    Update page.js

--- app/page.js
+++ app/page.js
@@ -390,18 +390,8 @@ function AnimatedCounter({ target, suffix = '' }) {
   return <span ref={ref}>{val}{suffix}</span>
 }
 
-function TextReveal({ children, className = '', delay = 0 }) {
-  return (
-    <motion.div
-      initial={{ opacity: 0, y: 30 }}
-      whileInView={{ opacity: 1, y: 0 }}
-      viewport={{ once: true, margin: '-80px' }}
-      transition={{ duration: 0.8, delay, ease: [0.22, 1, 0.36, 1] }}
-      className={className}
-    >
-      {children}
-    </motion.div>
-  )
+function TextReveal({ children, className = '' }) {
+  return <div className={className}>{children}</div>
 }
 
 /* ---------------- SECTIONS ---------------- */
